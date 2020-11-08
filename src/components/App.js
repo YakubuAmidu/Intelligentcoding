03f2a393fc3c5d54cd09c76155ddd7776b39a9d2
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-08 06:08:13 -0800

    Commiting changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import HomeScreen from "../screens/HomeScreen";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>App</h1>
+        <HomeScreen />
       </div>
     );
   }
