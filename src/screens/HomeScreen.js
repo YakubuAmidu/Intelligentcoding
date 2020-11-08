8073366ea8eaed6ba8db72f5c8b343fd84bb0076
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-08 07:13:48 -0800

    Commiting changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "../components/App.css";
+import Header from "../components/Header";
 
 const HomeScreen = () => {
   return (
     <div className="HomeScreen_section">
+      <Header />
       <h1>HomeScreen</h1>
     </div>
   );
