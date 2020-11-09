eeb0aa63a7ba394852c7cf730239130a40bcc28c
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-09 01:36:28 -0800

    Commiting changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -9,16 +9,27 @@ const HomeScreen = () => {
       <Header />
       <div className="Header_section">
         <div className="Header_image">
-          <img src={Imageone} alt="img" className="Imagetwo" />
+          <img src={Imageone} alt="img" className="Imagetone" />
         </div>
-        <h1 className="Heading_primary">
-          <span className="Heading_primary-main">
-            Lorem ipsum dolor sit amet, consectetur
-          </span>
-          <span className="Heading_primary-sub">
-            Lorem ipsum dolor sit amet, consectetur
-          </span>
-        </h1>
+
+        <div className="Heading_primary">
+          <p className="Heading_primary-main">
+            Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet,
+            consectetur
+          </p>
+          <p className="Heading_primary-sub">
+            Lorem ipsum dolor sit amet, consectetur Lorem ipsum dolor sit amet,
+            consectetur
+          </p>
+        </div>
+
+        <div className="Heading_secondary">
+          <p className="Heading_secondary-main">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et
+          </p>
+        </div>
+        <button>Start</button>
       </div>
     </div>
   );
