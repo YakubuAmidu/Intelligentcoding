553b8022d17f2b38860af0003f9dce1f7b2fe467
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-10 01:09:51 -0800

    Commiting changes

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -7,63 +7,63 @@ const HomeScreen = () => {
   return (
     <div className="HomeScreen_section">
       <Header />
-      <div className="Header_primary">
-        <h1>Hellooo! Welcome to Intelligent-coding</h1>
-        <h4>
+      <div className="Header-primary">
+        <h1 className="Header-main">Hellooo! Welcome to Intelligent-coding</h1>
+        <h4 className="Header-sub">
           Start from beginning to advance. You guaranteed to become a pro.
         </h4>
       </div>
 
       <div>
-        <h1>Courses</h1>
+        <h1 className="Header-course">Courses</h1>
         <Row>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Html">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Css">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Grid">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Flexbox">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
         </Row>
         <Row>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Sass">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Bootstrap">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Reactjs">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
           <Col>
-            <div>
-              <p>React native</p>
+            <div className="Skills">
+              <p className="Reactnative">React native</p>
               <Button>Begin course</Button>
             </div>
           </Col>
