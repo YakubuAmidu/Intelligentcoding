5095aca9ca1104b19c5b33ccb5c651d33fcc3f5f
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-11 04:37:47 -0800

    Commiting changes

--- src/screens/ContactScreen.js
+++ src/screens/ContactScreen.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ContactScreen = () => {
   return (
-    <div>
-      <h1>ContactScreen()</h1>
+    <div className="Contact-screen">
+      <p className="Header-contact">Connect with me</p>
+      <div className="Header-icons">
+        <Link to="/" className="Header-link">
+          <i className="fas fa-envelope Icons"></i>
+        </Link>
+        <Link to="/" className="Header-link">
+          <i className="fab fa-github-square Icons"></i>
+        </Link>
+        <Link to="/" className="Header-link">
+          <i className="fab fa-linkedin Icons"></i>
+        </Link>
+        <Link to="/" className="Header-link">
+          <i class="fab fa-instagram-square Icons"></i>
+        </Link>
+        <Link to="/" className="Header-link">
+          <i className="fab fa-facebook-square Icons"></i>
+        </Link>
+        <Link to="/" className="Header-link">
+          <i className="fab fa-twitter-square Icons"></i>
+        </Link>
+      </div>
     </div>
   );
 };
