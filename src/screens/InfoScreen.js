9a63af7ea86de8be30b1572d9e88a57fd9573092
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-11 22:40:44 -0800

    Commiting changes

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -4,11 +4,10 @@ import { Container, Col, Row } from "react-bootstrap";
 const InfoScreen = () => {
   return (
     <Container fluid className="InfoScreen-section">
-      <h1>InfoScreen</h1>
       <Row>
         <Col sm={6}>
           <div className="Info-icons">
-            <i className="Icons" class="fab fa-html5"></i>
+            <p className="Icons html">HTML</p>
             <p className="Info-paragraph">
               HTML stands for Hyper Text Markup Language. HTML is the standard
               markup language for creating Web pages. HTML describes the
@@ -19,7 +18,20 @@ const InfoScreen = () => {
         </Col>
         <Col sm={6}>
           <div className="Info-icons">
-            <i className="Icons" class="fab fa-bootstrap"></i>
+            <p className="Icons css">CSS</p>
+            <p className="Info-paragraph">
+              CSS stands for Cascading Style Sheets with an emphasis placed on
+              “Style.” While HTML is used to structure a web document (defining
+              things like headlines and paragraphs, and allowing you to embed
+              images, video, and other media), CSS comes through and specifies
+              your document’s style—page layouts, colors, and fonts are all
+              determined with CSS.
+            </p>
+          </div>
+        </Col>
+        <Col sm={6}>
+          <div className="Info-icons">
+            <p className="Icons bootstrap">BOOTSTRAP</p>
             <p className="Info-paragraph">
               What is Bootstrap? Bootstrap is a potent front-end framework used
               to create modern websites and web apps. It's open-source and free
@@ -31,23 +43,13 @@ const InfoScreen = () => {
         </Col>
         <Col sm={6}>
           <div className="Info-icons">
-            <i className="Icons" class="fab fa-bootstrap"></i>
+            <p className="Icons flexbox">FLEXBOX</p>
             <p className="Info-paragraph">
-              HTML stands for Hyper Text Markup Language. HTML is the standard
-              markup language for creating Web pages. HTML describes the
-              structure of a Web page. HTML consists of a series of elements.
-              HTML elements tell the browser how to display the content.
-            </p>
-          </div>
-        </Col>
-        <Col sm={6}>
-          <div className="Info-icons">
-            <i className="Icons" class="fab fa-bootstrap"></i>
-            <p className="Info-paragraph">
-              HTML stands for Hyper Text Markup Language. HTML is the standard
-              markup language for creating Web pages. HTML describes the
-              structure of a Web page. HTML consists of a series of elements.
-              HTML elements tell the browser how to display the content.
+              Flexbox is a layout model that allows elements to align and
+              distribute space within a container. Using flexible widths and
+              heights, elements can be aligned to fill a space or distribute
+              space between elements, which makes it a great tool to use for
+              responsive design systems
             </p>
           </div>
         </Col>
