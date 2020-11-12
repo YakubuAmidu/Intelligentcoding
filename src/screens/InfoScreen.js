d9de3c343e5e8630177f58b598c70ad36b1e6e8a
Yakubu Amidu <yakubu.amidu@yahoo.com>
2020-11-11 22:08:12 -0800

    Commiting changes

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -1,10 +1,58 @@
 import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
 
 const InfoScreen = () => {
   return (
-    <div>
+    <Container fluid className="InfoScreen-section">
       <h1>InfoScreen</h1>
-    </div>
+      <Row>
+        <Col sm={6}>
+          <div className="Info-icons">
+            <i className="Icons" class="fab fa-bootstrap"></i>
+            <p>
+              HTML stands for Hyper Text Markup Language. HTML is the standard
+              markup language for creating Web pages. HTML describes the
+              structure of a Web page. HTML consists of a series of elements.
+              HTML elements tell the browser how to display the content.
+            </p>
+          </div>
+        </Col>
+        <Col sm={6}>
+          <div className="Info-icons">
+            <i className="Icons" class="fab fa-bootstrap"></i>
+            <p>
+              What is Bootstrap? Bootstrap is a potent front-end framework used
+              to create modern websites and web apps. It's open-source and free
+              to use, yet features numerous HTML and CSS templates for UI
+              interface elements such as buttons and forms. Bootstrap also
+              supports JavaScript extensions.
+            </p>
+          </div>
+        </Col>
+        <Col sm={6}>
+          <div className="Info-icons">
+            <i className="Icons" class="fab fa-bootstrap"></i>
+            <p>
+              HTML stands for Hyper Text Markup Language. HTML is the standard
+              markup language for creating Web pages. HTML describes the
+              structure of a Web page. HTML consists of a series of elements.
+              HTML elements tell the browser how to display the content.
+            </p>
+          </div>
+        </Col>
+        <Col sm={6}>
+          <div className="Info-icons">
+            <i className="Icons" class="fab fa-bootstrap"></i>
+            <p>
+              HTML stands for Hyper Text Markup Language. HTML is the standard
+              markup language for creating Web pages. HTML describes the
+              structure of a Web page. HTML consists of a series of elements.
+              HTML elements tell the browser how to display the content.
+            </p>
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
